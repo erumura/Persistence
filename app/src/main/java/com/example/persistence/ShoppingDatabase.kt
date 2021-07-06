b8a0ce699db0bf2faa09f34139f7db0640fb4086
erumura <davidenawore@gmail.com>
2021-07-06 05:33:21 -1100

    dataBase_creation

---
+++ app/src/main/java/com/example/persistence/ShoppingDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.persistence
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database (entities = arrayOf(ShoppingModel::class),
+version = 1
+    )
+
+abstract class ShoppingDatabase: RoomDatabase() {
+}
